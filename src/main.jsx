37fa989086eedc0dce62707bdafa468ef4f39719
Jean Lima <jcoliveiralima19@gmail.com>
2026-08-22 13:29:43 -0300

    Testando o painel de tema

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Theme>
       <App />
+      <ThemePanel />
     </Theme>
   </StrictMode>,
 );

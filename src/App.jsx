03a2b58aa39355b25f435ce62268f7b65d3df02b
Jean Lima <jcoliveiralima19@gmail.com>
2026-08-22 13:29:20 -0300

    Testando componentes com a estilização do tema do Radix UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { Button, Flex, Text } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 function App() {
-
   return (
     <>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
